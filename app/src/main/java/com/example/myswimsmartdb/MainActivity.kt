6e2be8c57e0a54c96d2c21f5c5ae57aa6ee7536c
RIKE_BENTHEIM <rike.bentheim@gmx.de>
2024-05-31 15:10:39 +0200

    Läuft aufruf von der Manin

--- app/src/main/java/com/example/myswimsmartdb/MainActivity.kt
+++ app/src/main/java/com/example/myswimsmartdb/MainActivity.kt
@@ -3,19 +3,11 @@ package com.example.myswimsmartdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.myswimsmartdb.ui.Composable.DrawerContent
-import com.example.myswimsmartdb.ui.navigation.AppNavigation
+import com.example.myswimsmartdb.ui.screens.HomeScreen
 import com.example.myswimsmartdb.ui.theme.AppTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,36 +18,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
     AppTheme {
         val navController = rememberNavController()
-        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                DrawerContent(navController = navController)
-            }
-        ) {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = { Text("MySwimSmartDB") },
-                        navigationIcon = {
-                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                                Icon(Icons.Filled.Menu, contentDescription = "Menu")
-                            }
-                        }
-                    )
-                }
-            ) { paddingValues ->
-                Box(modifier = Modifier.padding(paddingValues)) {
-                    AppNavigation(navController = navController)
-                }
-            }
-        }
+        HomeScreen(navController = navController)
     }
 }
