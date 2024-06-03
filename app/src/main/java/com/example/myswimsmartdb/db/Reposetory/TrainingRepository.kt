00c2214334e25d781e47f765dd60ec8e628ebfc9
RIKE_BENTHEIM <rike.bentheim@gmx.de>
2024-06-03 10:30:45 +0200

    Termi berarbeiten Past

--- app/src/main/java/com/example/myswimsmartdb/db/Reposetory/TrainingRepository.kt
+++ app/src/main/java/com/example/myswimsmartdb/db/Reposetory/TrainingRepository.kt
@@ -34,25 +34,37 @@ class TrainingRepository(context: Context) {
         }
         db.insert(DatabaseHelper.TABLE_ANWESENHEIT, null, values)
     }
+
     fun getTrainingsByKursId(kursId: Int): List<Training> {
         val db = dbHelper.readableDatabase
         val query = """
-        SELECT t.* FROM ${DatabaseHelper.TABLE_TRAINING} t
-        INNER JOIN ${DatabaseHelper.TABLE_KURS_TRAINING} kt
-        ON t.TRAINING_ID = kt.KURS_TRAINING_TRAINING_ID
-        WHERE kt.KURS_TRAINING_KURS_ID = ?
-    """
+            SELECT t.* FROM ${DatabaseHelper.TABLE_TRAINING} t
+            INNER JOIN ${DatabaseHelper.TABLE_KURS_TRAINING} kt
+            ON t.TRAINING_ID = kt.KURS_TRAINING_TRAINING_ID
+            WHERE kt.KURS_TRAINING_KURS_ID = ?
+        """
         val cursor = db.rawQuery(query, arrayOf(kursId.toString()))
+
         val trainings = mutableListOf<Training>()
-        with(cursor) {
-            while (moveToNext()) {
-                val trainingId = getInt(getColumnIndexOrThrow("TRAINING_ID"))
-                val datum = getString(getColumnIndexOrThrow("TRAINING_DATUM"))
-                val bemerkung = getString(getColumnIndexOrThrow("TRAINING_BEMERKUNG"))
-                trainings.add(Training(trainingId, datum, bemerkung))
-            }
+        while (cursor.moveToNext()) {
+            val trainingId = cursor.getInt(cursor.getColumnIndexOrThrow("TRAINING_ID"))
+            val datum = cursor.getString(cursor.getColumnIndexOrThrow("TRAINING_DATUM"))
+            val bemerkung = cursor.getString(cursor.getColumnIndexOrThrow("TRAINING_BEMERKUNG"))
+
+            val training = Training(trainingId, datum, bemerkung)
+            trainings.add(training)
         }
         cursor.close()
         return trainings
+    }
+
+    fun deleteTraining(trainingId: Int) {
+        val db = dbHelper.writableDatabase
+        db.delete(DatabaseHelper.TABLE_TRAINING, "TRAINING_ID = ?", arrayOf(trainingId.toString()))
+    }
+
+    fun deleteAnwesenheitByTrainingId(trainingId: Int) {
+        val db = dbHelper.writableDatabase
+        db.delete(DatabaseHelper.TABLE_ANWESENHEIT, "ANWESENHEIT_TRAINING_ID = ?", arrayOf(trainingId.toString()))
     }
 }
--- app/src/main/java/com/example/myswimsmartdb/ui/screens/KursScreen.kt
+++ app/src/main/java/com/example/myswimsmartdb/ui/screens/KursScreen.kt
@@ -1,5 +1,7 @@
 package com.example.myswimsmartdb.ui.screens
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -13,7 +15,10 @@ import com.example.myswimsmartdb.db.KursRepository
 import com.example.myswimsmartdb.db.MitgliedRepository
 import com.example.myswimsmartdb.db.TrainingRepository
 import com.example.myswimsmartdb.db.entities.Kurs
+import com.example.myswimsmartdb.db.entities.Training
 import com.example.myswimsmartdb.ui.Composable.StringSelectionDropdown
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun KursScreen(navController: NavHostController) {
@@ -24,28 +29,127 @@ fun KursScreen(navController: NavHostController) {
 
     val courses = kursRepository.getAllKurseWithDetails()
     var selectedCourse by remember { mutableStateOf<Kurs?>(null) }
+    var selectedTraining by remember { mutableStateOf<Training?>(null) }
+    var editMode by remember { mutableStateOf(false) }
+    var trainings by remember { mutableStateOf(emptyList<Training>()) }
+    var message by remember { mutableStateOf("") }
 
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)) {
-        StringSelectionDropdown(
-            label = "Bitte einen Kurs auswählen:",
-            options = courses.map { it.name },
-            selectedOption = selectedCourse?.name ?: "",
-            onOptionSelected = { courseName ->
-                selectedCourse = courses.find { it.name == courseName }
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        selectedCourse?.let { course ->
-            CourseDetails(
-                course = course,
-                trainingRepository = trainingRepository,
-                mitgliedRepository = mitgliedRepository
+        if (!editMode) {
+            StringSelectionDropdown(
+                label = "Bitte einen Kurs auswählen:",
+                options = courses.map { it.name },
+                selectedOption = selectedCourse?.name ?: "",
+                onOptionSelected = { courseName ->
+                    selectedCourse = courses.find { it.name == courseName }
+                    selectedTraining = null
+                    selectedCourse?.let {
+                        trainings = trainingRepository.getTrainingsByKursId(it.id)
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            selectedCourse?.let { course ->
+                Button(
+                    onClick = {
+                        editMode = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Kurs Bearbeiten")
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                CourseDetails(
+                    course = course,
+                    trainingRepository = trainingRepository,
+                    mitgliedRepository = mitgliedRepository
+                )
+            }
+        } else {
+            selectedCourse?.let { course ->
+                val mitglieder = mitgliedRepository.getMitgliederByKursId(course.id)
+
+                StringSelectionDropdown(
+                    label = "Bitte einen Termin auswählen:",
+                    options = trainings.map { it.datumString },
+                    selectedOption = selectedTraining?.datumString ?: "",
+                    onOptionSelected = { trainingDatum ->
+                        selectedTraining = trainings.find { it.datumString == trainingDatum }
+                        message = ""
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                selectedTraining?.let {
+                    Button(
+                        onClick = {
+                            selectedTraining?.let { training ->
+                                trainingRepository.deleteTraining(training.id)
+                                trainingRepository.deleteAnwesenheitByTrainingId(training.id)
+                                selectedTraining = null
+                                trainings = trainingRepository.getTrainingsByKursId(course.id)
+                                message = "Der Termin wurde gelöscht."
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Termin Löschen")
+                    }
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+
+                if (message.isNotEmpty()) {
+                    Text(text = message, style = MaterialTheme.typography.bodyLarge, color = MaterialTheme.colorScheme.primary)
+                }
+
+                val dateFormat = remember { SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()) }
+                val calendar = remember { Calendar.getInstance() }
+                var selectedDate by remember { mutableStateOf("") }
+
+                Button(
+                    onClick = {
+                        val datePickerDialog = DatePickerDialog(
+                            context,
+                            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                                calendar.set(year, month, dayOfMonth)
+                                selectedDate = dateFormat.format(calendar.time)
+                                val newTraining = Training(0, selectedDate, "")
+                                val newTrainingId = trainingRepository.insertTraining(newTraining, course.id)
+                                mitglieder.forEach { mitglied ->
+                                    trainingRepository.insertAnwesenheit(mitglied.id, newTrainingId)
+                                }
+                                trainings = trainingRepository.getTrainingsByKursId(course.id)
+                                message = "Neuer Termin wurde hinzugefügt."
+                            },
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH)
+                        )
+                        datePickerDialog.show()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Neuen Termin hinzufügen")
+                }
+
+                StringSelectionDropdown(
+                    label = "Bitte ein Mitglied auswählen:",
+                    options = mitglieder.map { "${it.vorname} ${it.nachname}" },
+                    selectedOption = "",
+                    onOptionSelected = { /* Handle member selection */ },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
@@ -68,15 +172,23 @@ fun CourseDetails(
         Text(text = "Level: ${course.levelName}", style = MaterialTheme.typography.bodyLarge)
         Spacer(modifier = Modifier.height(10.dp))
 
-        Text(text = "Trainings:", style = MaterialTheme.typography.bodyLarge)
-        trainings.forEach { training ->
-            Text(text = training.datumString, style = MaterialTheme.typography.bodySmall)
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(text = "Members:", style = MaterialTheme.typography.bodyLarge)
-        mitglieder.forEach { member ->
-            Text(text = "${member.vorname} ${member.nachname}", style = MaterialTheme.typography.bodySmall)
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)) {
+                Text(text = "Trainings:", style = MaterialTheme.typography.bodyLarge)
+                trainings.forEach { training ->
+                    Text(text = training.datumString, style = MaterialTheme.typography.bodySmall)
+                }
+            }
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)) {
+                Text(text = "Mitglieder:", style = MaterialTheme.typography.bodyLarge)
+                mitglieder.forEach { member ->
+                    Text(text = "${member.vorname} ${member.nachname}", style = MaterialTheme.typography.bodySmall)
+                }
+            }
         }
     }
 }
